fca2e8113d746110d24fb10596300d2f37fbc58c
Micah Silverman <micah@afitnerd.com>
2019-05-29 16:49:33 -0700

    refactored OAuth2User to OidcUser

--- oauth-okta/src/main/java/com/okta/springsecurityauth/WebController.java
+++ oauth-okta/src/main/java/com/okta/springsecurityauth/WebController.java
@@ -1,7 +1,7 @@
 package com.okta.springsecurityauth;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,20 +11,20 @@ public class WebController {
 
     @RequestMapping("/")
     @ResponseBody
-    public String home(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return "Welcome, " + oauth2User.getName();
+    public String home(@AuthenticationPrincipal OidcUser oidcUser) {
+        return "Welcome, " + oidcUser.getFullName();
     }
 
     @RequestMapping("/attributes")
     @ResponseBody
-    public String attributes(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return oauth2User.getAttributes().toString();
+    public String attributes(@AuthenticationPrincipal OidcUser oidcUser) {
+        return oidcUser.getAttributes().toString();
     }
 
     @RequestMapping("/authorities")
     @ResponseBody
-    public String authorities(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return oauth2User.getAuthorities().toString();
+    public String authorities(@AuthenticationPrincipal OidcUser oidcUser) {
+        return oidcUser.getAuthorities().toString();
     }
     
 }
